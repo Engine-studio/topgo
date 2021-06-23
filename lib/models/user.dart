0997edb477848611e7abceb2b64ae43821d44568
Dmitry Novikov <novikov.dm.al@gmail.com>
2021-06-23 05:45:06 +0300

    courier active orders tracker

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -10,24 +10,33 @@ class User with ChangeNotifier {
   Role role;
   bool authorized;
   bool working;
+  int orders;
 
   User()
       : role = Role.Administrator,
         authorized = false,
-        working = false;
+        working = false,
+        orders = 1;
 
   User.authorized()
       : role = Role.Courier,
         authorized = true,
-        working = true;
+        working = true,
+        orders = 1;
 
   User.nonAuthorized()
       : role = Role.Courier,
         authorized = false,
-        working = false;
+        working = false,
+        orders = 1;
 
   void work() {
     this.working = !this.working;
+    notifyListeners();
+  }
+
+  void add() {
+    this.orders += 1;
     notifyListeners();
   }
 }
--- lib/widgets/bottom_navbar.dart
+++ lib/widgets/bottom_navbar.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:topgo/models/user.dart';
 import 'package:topgo/styles.dart';
+import 'package:provider/provider.dart';
 
 class BottomNavBar extends StatefulWidget {
   final void Function(int) onPressed;
@@ -24,6 +26,9 @@ class _BottomNavBarState extends State<BottomNavBar> {
 
   @override
   Widget build(BuildContext context) {
+    int orders = context.read<User>().role == Role.Courier
+        ? context.watch<User>().orders
+        : -1;
     return Theme(
       data: ThemeData(
         brightness: Brightness.light,
@@ -50,22 +55,57 @@ class _BottomNavBarState extends State<BottomNavBar> {
             selectedFontSize: 0,
             unselectedFontSize: 0,
             items: icons
-                .map((icon) => BottomNavigationBarItem(
-                      icon: ShaderMask(
-                        shaderCallback: _currentIndex == icons.indexOf(icon)
-                            ? (bounds) => GrdStyle.select.createShader(bounds)
-                            : (bounds) => GrdStyle()
-                                .panelGradient(context)
-                                .createShader(bounds),
-                        child: Image.asset(
-                          'assets/icons/$icon.png',
-                          width: 30,
-                          height: 30,
-                          color: Colors.white,
+                .map(
+                  (icon) => BottomNavigationBarItem(
+                    icon: Stack(
+                      clipBehavior: Clip.none,
+                      children: [
+                        ShaderMask(
+                          shaderCallback: _currentIndex == icons.indexOf(icon)
+                              ? (bounds) => GrdStyle.select.createShader(bounds)
+                              : (bounds) => GrdStyle()
+                                  .panelGradient(context)
+                                  .createShader(bounds),
+                          child: Image.asset(
+                            'assets/icons/$icon.png',
+                            width: 30,
+                            height: 30,
+                            color: Colors.white,
+                          ),
                         ),
-                      ),
-                      label: icon,
-                    ))
+                        ...icon == 'rocket' && orders > 0
+                            ? [
+                                Positioned(
+                                  left: 24,
+                                  bottom: 15,
+                                  child: Container(
+                                    width: 22,
+                                    height: 22,
+                                    decoration: BoxDecoration(
+                                      shape: BoxShape.circle,
+                                      gradient: _currentIndex ==
+                                              icons.indexOf(icon)
+                                          ? GrdStyle.select
+                                          : GrdStyle().panelGradient(context),
+                                    ),
+                                    child: Center(
+                                      child: Text(
+                                        '$orders',
+                                        style:
+                                            TxtStyle.selectedSmallText.copyWith(
+                                          color: ClrStyle.lightBackground,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ]
+                            : [],
+                      ],
+                    ),
+                    label: icon,
+                  ),
+                )
                 .toList(),
             onTap: (index) {
               setState(() {
